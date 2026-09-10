54abfb1fc99fd04e80a24ee327437f8d331a0852
clickidev <clicki3710@kw.ac.kr>
2026-09-10 11:23:36 +0900

    p3: scores pipeline

--- p3_scores.js
+++ p3_scores.js
@@ -13,7 +13,28 @@ const scores = Array.from({ length: 20 }, () => Math.floor(Math.random() * 101))
 console.log("start:", scores);
 
 // (a)
+const odds = scores.filter(x => x%2 !==0);
+console.log("Odds: ", odds);
+
+const doubled = odds.map(x => x*2);
+console.log("Doubled: ", doubled);
+
+const sorted = doubled.sort((a, b) => b - a);
+console.log("Sorted: ", sorted);
+
+const f_half = sorted.slice(0, Math.ceil(sorted.length/2));
+console.log("First half: ", f_half);
 
 // (b)
+const result = scores
+.filter(x => x%2 !==0)
+.map(x => x*2)
+.sort((a, b) => b - a)
+.slice(0, Math.ceil(sorted.length/2));
+
+console.log("Result: ", result);
 
 // (c)
+console.log("Scores: ", scores);
+
+//filter는 non-destructive한 메소드이므로 변화x
